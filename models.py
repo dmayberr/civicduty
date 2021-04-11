ade5185a28c6ea76d4817103dc6a972e9f5a5ad1
Darrell Mayberry <dmayberr@gmail.com>
2021-04-11 18:57:02 -0400

    Created User model and bcrypt auth

---
+++ models.py
@@ -0,0 +1,91 @@
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+
+class User(db.Model):
+    """Users"""
+
+    __tablename__ = 'Users'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    username = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    password = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    email = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    residentState = db.Column(
+        db.Text,
+        nullable=False,        
+    )
+
+    residentCity = db.Column(
+        db.Text,
+        nullable=False,    
+    )
+
+    residentStreetAddress = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    @classmethod
+    def signup(cls, username, password, email, residentState, residentCity, residentStreetAddress):
+        """Register user and hashes password."""
+
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
+
+        user = User(
+            username=username,
+            password=password,
+            email=email,
+            residentState=residentState,
+            residentCity=residentCity,
+            residentStreetAddress=residentStreetAddress,
+        )
+
+        db.session.add(user)
+        return username
+    
+    @classmethod
+    def authenticate(cls, username, password):
+        """Find user with `username` and `password`.
+
+        This is a class method (call it on the class, not an individual user.)
+        It searches for a user whose password hash matches this password
+        and, if it finds such a user, returns that user object.
+
+        If can't find matching user (or if password is wrong), returns False.
+        """
+
+        user = cls.query.filter_by(username=username).first()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+
+        return False
+
+def connect_db(app):
+    """Connect this database to provided Flask app."""
+
+    db.app = app
+    db.init_app(app)
